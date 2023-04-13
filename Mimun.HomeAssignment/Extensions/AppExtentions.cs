cedd56b4c061b355e226884b43bc499ee975fe98
mikehac <mike.hatskevich@gmail.com>
2023-04-13 22:51:23 +0300

    register db and repositories

---
+++ Mimun.HomeAssignment/Extensions/AppExtentions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mimun.HomeAssignment.Models;
+using Mimun.HomeAssignment.Repository;
+
+namespace Mimun.HomeAssignment.Extensions
+{
+    public static class AppExtentions
+    {
+        public static void RegisterDb(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<MimunDbContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("ShoppingDb"));
+            });
+        }
+
+        public static void RegisterRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+        }
+    }
+}
--- Mimun.HomeAssignment/Program.cs
+++ Mimun.HomeAssignment/Program.cs
@@ -1,11 +1,21 @@
+using AutoMapper;
+using Mimun.HomeAssignment.Extensions;
+using Mimun.HomeAssignment.ServicesConfig;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.RegisterDb(builder.Configuration);
+builder.Services.RegisterRepositories();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+IMapper mapper = MapperConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+
 
 var app = builder.Build();
 
@@ -21,5 +31,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors(x =>
+    x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin()
+);
 
 app.Run();
