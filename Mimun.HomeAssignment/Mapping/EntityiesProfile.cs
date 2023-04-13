4423b3bee4067acdca5af1a07d777c30c4dc5b15
mikehac <mike.hatskevich@gmail.com>
2023-04-13 22:49:58 +0300

    Add autoMapping

---
+++ Mimun.HomeAssignment/Mapping/EntityiesProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Mimun.HomeAssignment.DTOs;
+using Mimun.HomeAssignment.Models;
+
+namespace Mimun.HomeAssignment.Mapping
+{
+    public class EntityiesProfile : Profile
+    {
+        public EntityiesProfile()
+        {
+            CreateMap<Customer, CustomerDto>().ReverseMap();
+            CreateMap<Contract, ContractDto>().ReverseMap();
+        }
+    }
+}
---
+++ Mimun.HomeAssignment/ServicesConfig/MapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Mimun.HomeAssignment.Mapping;
+
+namespace Mimun.HomeAssignment.ServicesConfig
+{
+    public class MapperConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var MappingConfig = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new EntityiesProfile());
+            });
+            return MappingConfig;
+        }
+    }
+}
