231813a373e3823d4d2b45f96ade723a5943cd42
mikehac <mike.hatskevich@gmail.com>
2023-04-13 22:53:02 +0300

    adding a controller, connection string to config file

---
+++ Mimun.HomeAssignment/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mimun.HomeAssignment.Repository;
+
+namespace Mimun.HomeAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string idNumber) 
+        {
+            var response = await _customerRepository.GetByIdNumber(idNumber);
+            return Ok(response);
+        }
+    }
+}
